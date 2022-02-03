82f953e66a8c0f0a4f669b92147023ac75c8e58c
Rifqi Alfian <merembablas@gmail.com>
2022-02-03 13:00:39 +0700

    fix: change readme

--- app/Services/IndodaxService.php
+++ app/Services/IndodaxService.php
@@ -18,43 +18,43 @@ class IndodaxService
 
     public function info()
     {
-        $params = [ 'method' => 'getInfo' ];
+        $params = ['method' => 'getInfo'];
         return $this->_sendPrivate($params);
     }
 
     public function transactions()
     {
-        $params = [ 'method' => 'transHistory' ];
+        $params = ['method' => 'transHistory'];
         return $this->_sendPrivate($params);
     }
 
     public function openOrders($params)
     {
-        $params = [ 'method' => 'openOrders' ] + $params;
+        $params = ['method' => 'openOrders'] + $params;
         return $this->_sendPrivate($params);
     }
 
     public function createOrder($params)
     {
-        $params = [ 'method' => 'trade' ] + $params;
+        $params = ['method' => 'trade'] + $params;
         return $this->_sendPrivate($params);
     }
 
     public function cancelOrder($params)
     {
-        $params = [ 'method' => 'cancelOrder' ] + $params;
+        $params = ['method' => 'cancelOrder'] + $params;
         return $this->_sendPrivate($params);
     }
 
     public function orderHistory($params)
     {
-        $params = [ 'method' => 'orderHistory' ] + $params;
+        $params = ['method' => 'orderHistory'] + $params;
         return $this->_sendPrivate($params);
     }
 
     public function tradeHistory($params)
     {
-        $params = [ 'method' => 'tradeHistory' ] + $params;
+        $params = ['method' => 'tradeHistory'] + $params;
         $data = $this->_sendPrivate($params);
 
         return $data['trades'];
@@ -62,7 +62,7 @@ class IndodaxService
 
     public function tickerAll()
     {
-        $params = [ 'path' => '/ticker_all' ];
+        $params = ['path' => '/ticker_all'];
         $data = $this->_sendPublic($params);
 
         return isset($data['tickers']) ? $data['tickers'] : false;
@@ -70,7 +70,7 @@ class IndodaxService
 
     public function ticker($pair)
     {
-        $params = [ 'path' => '/ticker/' . $pair ];
+        $params = ['path' => '/ticker/' . $pair];
         $data = $this->_sendPublic($params);
 
         return isset($data['ticker']) ? $data['ticker'] : false;
@@ -80,33 +80,34 @@ class IndodaxService
     {
         $data = [];
         foreach ($pairs as $pair) {
-            $params = [ 'path' => '/trades/' . $pair ];
+            $params = ['path' => '/trades/' . $pair];
             $data[$pair] = $this->_sendPublic($params);
         }
-        
+
         return $data;
     }
 
     public function depth($pair)
     {
-        $params = [ 'path' => '/depth/' . $pair ];
+        $params = ['path' => '/depth/' . $pair];
         $data = $this->_sendPublic($params);
-        
+
         return $data;
     }
 
     public function pairs()
     {
-        $params = [ 'path' => '/pairs' ];
+        $params = ['path' => '/pairs'];
         $result = $this->_sendPublic($params);
 
         return $result;
     }
 
-    public function history($params) {
+    public function history($params)
+    {
         $tvUrl = str_replace('api', '', $this->public_base_url) . 'tradingview/history';
-        $response = Http::get($tvUrl . '?symbol=' . $params['symbol'] . '&resolution=' 
-            . $params['resolution'] . '&from=' . $params['from'] 
+        $response = Http::get($tvUrl . '?symbol=' . $params['symbol'] . '&resolution='
+            . $params['resolution'] . '&from=' . $params['from']
             . '&to=' . $params['to']);
 
         if (!$response->successful()) {
@@ -117,20 +118,21 @@ class IndodaxService
         if (!isset($backtestData['s']) || $backtestData['s'] != 'ok') {
             return [];
         }
-        
+
         return $backtestData;
     }
 
-    private function _sendPrivate($params) {
+    private function _sendPrivate($params)
+    {
         $date = new \DateTime();
         $now = $date->getTimestamp() * 1000;
         $nextSeconds = $now + $this->rec_time;
 
         $data = [
-	        'timestamp' => $now,
-	        'recvWindow' => $nextSeconds
-	    ] + $params;
-        
+            'timestamp' => $now,
+            'recvWindow' => $nextSeconds
+        ] + $params;
+
         $postData = http_build_query($data, '', '&');
         $sign = hash_hmac('sha512', $postData, config('app.indodax_secret_key'));
 
@@ -154,7 +156,8 @@ class IndodaxService
         }
     }
 
-    private function _sendPublic($params) {
+    private function _sendPublic($params)
+    {
         //$response = Http::withOptions(['proxy' => config('services.proxy_url')])->get($this->public_base_url . $params['path']);
         $response = Http::get($this->public_base_url . $params['path']);
 
